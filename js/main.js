cb96651ca81228692cd83032b6ae85889c45a8c0
fleosan <50647063+fleosan@users.noreply.github.com>
2023-08-11 22:39:45 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,15 +8,16 @@ $(document).ready(function(){
 });
     
  
+$('.owl-carousel').on('changed.owl.carousel', function(event) {
+  var currentSlide = event.item.index;
+  $('.menu a.active').removeClass('active');
+  $('.menu a[data-slide="' + currentSlide + '"]').addClass('active');
+});
+
 $('.menu a').click(function(e) {
   e.preventDefault();
   var slide = $(this).data('slide');
   $('.owl-carousel').trigger('to.owl.carousel', slide);
-    
-$('.menu a.active').removeClass('active');
-  
-  // Add active class to clicked anchor
-  $(this).addClass('active');
 });
     
     
