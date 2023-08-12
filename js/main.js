c27d7001a3924ec4b84b8b2893689c6e119e8696
fleosan <50647063+fleosan@users.noreply.github.com>
2023-08-12 12:41:35 +0100

    .

--- js/main.js
+++ js/main.js
@@ -5,19 +5,38 @@ $(document).ready(function(){
     loop: true,
     items:1,
     smartSpeed:450,
+    dots:false,
+      
 });
     
- 
-$('.menu a').click(function(e) {
+
+$('.menu .navbar-nav .nav-item a').click(function(e) {
   e.preventDefault();
   var slide = $(this).data('slide');
   $('.owl-carousel').trigger('to.owl.carousel', slide);
     
-$('.menu a.active').removeClass('active');
+$('.menu .navbar-nav .nav-item a.active').removeClass('active');
   
   // Add active class to clicked anchor
   $(this).addClass('active');
 });
     
-    
+
+$('.owl-carousel').on('changed.owl.carousel', function(event) {
+  var current = event.item.index;
+
+
+
+
+$('.nav-item a').eq(current - 2).addClass('active').closest('.nav-item').siblings().find('a').removeClass('active');
+
+
+
 });
+
+ 
+
+
+});
+
+
